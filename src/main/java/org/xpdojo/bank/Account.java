f84795e4c76ba49f0bbf1d3662ce37036ce88144
David Goodall <fngopt@aol.com>
2021-09-15 13:22:41 +0100

    completed refactored!

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,15 +1,31 @@
 package org.xpdojo.bank;
+import java.util.Vector;
+import java.lang.Integer;
 
 public class Account {
-    int balance = 0;
+    Vector<Integer> deposits;
+
+    Account()
+    {
+        this.deposits = new Vector<>();
+    }
 
     void deposit(int amount)
     {
-        this.balance += amount;
+        this.deposits.add(amount);
     }
 
     int getBalance()
     {
+        if (this.deposits.size() == 0)
+        {
+            return 0;
+        }
+        int balance = 0;
+        for (Integer val : this.deposits)
+        {
+            balance += val;
+        }
         return balance;
     }
 }
